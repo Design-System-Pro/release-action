f646d818d097214b69524decff33e12dc3b84ef2
Tomas Francisco <4301103+tomasfrancisco@users.noreply.github.com>
2024-10-08 17:37:02 +0200

    fix the tokens path

--- src/generator/style-dictionary.ts
+++ src/generator/style-dictionary.ts
@@ -1,29 +1,29 @@
-import StyleDictionary from 'style-dictionary';
-import { hiddenFromPublishingFilter, releaseFilter } from './filters';
-import { recursiveFilter } from './filters/utils';
-import { ReleaseType } from './args';
-import { PlatformConfig } from 'style-dictionary/types';
+import StyleDictionary from "style-dictionary";
+import { hiddenFromPublishingFilter, releaseFilter } from "./filters";
+import { recursiveFilter } from "./filters/utils";
+import { ReleaseType } from "./args";
+import { PlatformConfig } from "style-dictionary/types";
 
 interface TransformOptions {
   tokensPath: string;
 }
 
 export async function transform({ tokensPath }: TransformOptions) {
-  const releases: ReleaseType[] = ['alpha', 'beta', 'stable'];
+  const releases: ReleaseType[] = ["alpha", "beta", "stable"];
   const styleDictionary = new StyleDictionary(
     {
-      source: [`../../${tokensPath}/**/*.json`],
+      source: [`${tokensPath}/**/*.json`],
       platforms: {
         ...releases.reduce<Record<string, PlatformConfig>>(
           (platforms, release) => ({
             ...platforms,
             [`css-${release}`]: {
-              transformGroup: 'css',
-              buildPath: `../../packages/${release}/css/`,
+              transformGroup: "css",
+              buildPath: `lib/${release}/css/`,
               files: [
                 {
-                  destination: '_variables.css',
-                  format: 'css/variables',
+                  destination: "_variables.css",
+                  format: "css/variables",
                   filter: (token, options) =>
                     recursiveFilter(token, options, [
                       hiddenFromPublishingFilter,
@@ -38,7 +38,7 @@ export async function transform({ tokensPath }: TransformOptions) {
       },
     },
     {
-      verbosity: 'verbose',
+      verbosity: "verbose",
     }
   );
 
