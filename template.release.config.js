30578379e2031d3ff24eac1920e6aa0a0c616383
Tomás Francisco <4301103+tomasfrancisco@users.noreply.github.com>
2024-10-24 16:14:53 +0200

    feat: improve action (#7)
    * feat: release in custom branch and add registry release
    
    * chore: run release on next
    
    * fix: expose node auth token
    
    * feat: add setup node version to action
    
    * feat: add registry url to action
    
    * chore: change package name and version

--- template.release.config.js
+++ template.release.config.js
@@ -1,9 +1,10 @@
 const config = {
-  branches: ["main"],
+  branches: [{{RELEASE_BRANCH}}],
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
+    "@semantic-release/npm",
     [
       "@semantic-release/git",
       {
